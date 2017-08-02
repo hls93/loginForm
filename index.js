1671bec67783c4ba05288803f1b030698cdf5d09
hls93 <hlshaw3@ncsu.edu>
2017-08-02 15:30:44 -0400

    adding comments

--- index.js
+++ index.js
@@ -52,8 +52,11 @@ app.use((req, res, next) => {
 });
 
 app.get('/', function(req, res) {
+  //if there isn't currently a user logged in...
   if (req.session.users.length === 0) {
+    //redirect to login page
     res.redirect('login')
+    //else send them to the home page
   } else {
     res.render('home', {
       info: req.session.users
@@ -62,8 +65,11 @@ app.get('/', function(req, res) {
 })
 
 app.get('/login', function(req, res) {
+  //if there is not a user logged in...
   if (req.session.users.length === 0) {
+    //stay on the login page
     res.render('login')
+    //else take them to the home page
   } else {
     res.redirect('/')
   }
@@ -71,24 +77,30 @@ app.get('/login', function(req, res) {
 
 
 app.post('/login/smerg', (req, res) => {
-
+//providing error messages if the user does not provide a name or password
   req.checkBody('userName', 'Must provide a User Name').notEmpty();
   req.checkBody('password', 'Must provide a password').notEmpty();
 
   req.getValidationResult().then(function(result){
     var errorsArray = result.array()
+    //if there is an error ie. greater than 0...
     if (errorsArray.length > 0) {
+      //take them back to the login page and display errors
       res.render('login', {
         errors: errorsArray
       })
+  //if there are no errors we need to check if this is a valid user
     } else {
+  //to do that we will iterate over the users.js file to make sure that username and password match key values that they have been assigned
     for (var i = 0; i < users.length; i++) {
+      //if it matches, redirect them to home and push the session info into the array
       if (req.body.userName === users[i].userName && req.body.password === users[i].password) {
         (req.session.users).push(users[i])
         res.redirect('/')
       }
     }
   }
+  //if none of the above is true, take them straight to the login page
   res.redirect('/login')
 })
 })
